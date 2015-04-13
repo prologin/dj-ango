dbd32711043101925ce14f288cddc660aad6621b
Zeletochoy <lecubi_a@epita.fr>
2015-04-13 21:51:00 +0200

    Migrate from sqlite to postgresql

--- settings.py
+++ settings.py
@@ -12,10 +12,10 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'DJ_Ango.db',
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'dj_ango',
+        'USER': 'dj_ango',               # Not used with sqlite3.
+        'PASSWORD': 'lepoulpepolonais',  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
